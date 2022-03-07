83ec504e7c53ca31c12c1c55213fbabd4a184085
Allexandre-Meneses <meloxandy@hotmail.com>
2022-03-07 16:42:56 -0300

    Alterei as configs da db

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ require('dotenv').config();
 const { DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_HOST } = process.env;
 
 module.exports = {
-    username: "postgres",
-    password: "123456",
-    database: "tryMusic",
-    host: "172.17.0.3",
+    username: "jlagjhpyivivwp",
+    password: "405f960cb22e7c004111c40a1f9afccd1bded71c4a629dae0fbb3dbaebf6e857",
+    database: "d8m4per4tl7i5j",
+    host: "ec2-54-156-110-139.compute-1.amazonaws.com",
     dialect: "postgres",
     define: {
         timestamps: true,
